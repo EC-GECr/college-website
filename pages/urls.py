e9eb5410909e8171b7bd2309df3d99f57f1ef0cb
sypai <7suyashbajpai@gmail.com>
2020-06-27 18:41:44 +0530

    [Update] Structured the HTML templates
    [Add] Institute, department, contact templates

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('institute', views.institute, name='institute'),
+    path('department', views.department, name='department'),
+    path('contact', views.contact, name='contact'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,14 @@ from django.shortcuts import render, redirect
 def index(request):
     return render(request, 'pages/index.html')
 
+
+def institute(request):
+    return render(request, 'pages/institute.html')
+
+
+def department(request):
+    return render(request, 'pages/department.html')
+
+
+def contact(request):
+    return render(request, 'pages/contact.html')
