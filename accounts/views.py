db8ceb6e24bf2f2279559a62a16977a191859090
sypai <7suyashbajpai@gmail.com>
2020-07-11 15:54:48 +0530

    Add Student Model, Creating registration view

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
+from student.models import Student
 
 
 def login(request):
@@ -31,6 +32,44 @@ def register(request):
 
 
 def register_form(request):
+    if request.method == 'POST':
+
+        first_name = request.POST['firstName']
+        last_name = request.POST['lastName']
+        email = request.POST['Email']
+        phone = request.POST['Phone']
+        roll = request.POST['Roll']
+        enroll = request.POST['Enroll']
+
+        board1 = request.POST['Board10']
+        tenthroll = request.POST['Tenthroll']
+        tenthcent = request.POST['Tenthcent']
+        board2 = request.POST['Board12']
+        twelthroll = request.POST['Twelthroll']
+        twelthcent = request.POST['Twelthcent']
+        petroll = request.POST['PETroll']
+        petrank = request.POST['PETrank']
+
+        fatherfirst = request.POST['FatherFirst']
+        fatherlast = request.POST['FatherLast']
+        fatherjob = request.POST['FatherJob']
+        fatherphone = request.POST['FatherPhone']
+        motherfirst = request.POST['MotherFirst']
+        motherlast = request.POST['MotherLast']
+        motherjob = request.POST['MotherJob']
+        motherphone = request.POST['MotherPhone']
+
+        dob = request.POST['dob']
+        sex = request.POST['sex']
+        blood = request.POST['blood']
+        height = request.POST['height']
+        weight = request.POST['weight']
+        aadhar = request.POST['aadhar']
+
+        student = Student(
+            fi
+        )
+
     return render(request, 'accounts/register_form.html')
 
 
--- student/models.py
+++ student/models.py
@@ -1,3 +1,62 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+    is_student = models.BooleanField(default=False)
+    is_faculty = models.BooleanField(default=True)
+
+    # Tell us about yourself
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    roll_number = models.CharField(max_length=20, unique=True)
+    username = None
+    enroll_number = models.CharField(max_length=20)
+
+    # Academic Details
+    tenth_board = models.IntegerField()
+    tenth_roll_number = models.CharField(max_length=50)
+    tenth_cent = models.CharField(max_length=10)
+
+    twelfth_board = models.IntegerField()
+    twelfth_roll_number = models.CharField(max_length=50)
+    twelfth_cent = models.CharField(max_length=10)
+
+    pet_roll_number = models.CharField(max_length=30)
+    pet_rank = models.CharField(max_length=8)
+
+    # Family details
+    father_first = models.CharField(max_length=30)
+    father_last = models.CharField(max_length=30)
+    father_job = models.CharField(max_length=30)
+    father_phone = models.CharField(max_length=30)
+
+    mother_first = models.CharField(max_length=30)
+    mother_last = models.CharField(max_length=30)
+    mother_job = models.CharField(max_length=30)
+    mother_phone = models.CharField(max_length=30)
+
+    # Personal Info
+    dob = models.DateField()
+    gender = models.IntegerField()
+    blood_group = models.CharField(max_length=5)
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    aadhar_number = models.CharField(max_length=30)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Student.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
